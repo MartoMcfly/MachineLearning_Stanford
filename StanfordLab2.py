95582da56317fe8bdec219ac90a62fd834f403c5
MartoMcfly <martinpelaez@hotmail.es>
2022-10-12 23:17:06 +0200

    Bug fixes: (5) Recharge info

---
+++ StanfordLab2.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# plt.style.use('./deeplearning.mplstyle')
+
+x_train = np.array([1.0, 2.0])           #(size in 1000 square feet)
+y_train = np.array([300.0, 500.0])  
+
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    cost_sum = 0 
+    for i in range(m): 
+        f_wb = w * x[i] + b   
+        cost = (f_wb - y[i]) ** 2  
+        cost_sum = cost_sum + cost  
+    total_cost = (1 / (2 * m)) * cost_sum  
+
+    return total_cost
+
+
+
+x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
+y_train = np.array([250, 300, 480,  430,   630, 730,])
+
+plt.close('all') 
+
+
