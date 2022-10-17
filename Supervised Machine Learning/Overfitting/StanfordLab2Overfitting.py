1f8b827df6d0493fa67509f02349c99863d3f9db
MartoMcfly <martinpelaez@hotmail.es>
2022-10-17 21:43:21 +0200

    Finish o First machine learning specialization

--- Supervised Machine Learning/Overfitting/StanfordLab2Overfitting.py
+++ Supervised Machine Learning/Overfitting/StanfordLab2Overfitting.py
@@ -1,42 +1,174 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
-from lab_utils_common import dlc, plot_data
-from plt_one_addpt_onclick import plt_one_addpt_onclick
-plt.style.use('./deeplearning.mplstyle')
+from plt_overfit import overfit_example, output
+from lab_utils_common import sigmoid
+np.set_printoptions(precision=8)
 
-x_train = np.array([0., 1, 2, 3, 4, 5])
-y_train = np.array([0,  0, 0, 1, 1, 1])
-X_train2 = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
-y_train2 = np.array([0, 0, 0, 1, 1, 1])
+def compute_cost_linear_reg(X, y, w, b, lambda_ = 1):
+    """
+    Computes the cost over all examples
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+    Returns:
+      total_cost (scalar):  cost 
+    """
 
-pos = y_train == 1
-neg = y_train == 0
+    m  = X.shape[0]
+    n  = len(w)
+    cost = 0.
+    for i in range(m):
+        f_wb_i = np.dot(X[i], w) + b                                   #(n,)(n,)=scalar, see np.dot
+        cost = cost + (f_wb_i - y[i])**2                               #scalar             
+    cost = cost / (2 * m)                                              #scalar  
+ 
+    reg_cost = 0
+    for j in range(n):
+        reg_cost += (w[j]**2)                                          #scalar
+    reg_cost = (lambda_/(2*m)) * reg_cost                              #scalar
+    
+    total_cost = cost + reg_cost                                       #scalar
+    return total_cost                                                  #scalar
 
-fig,ax = plt.subplots(1,2,figsize=(8,3))
-#plot 1, single variable
-ax[0].scatter(x_train[pos], y_train[pos], marker='x', s=80, c = 'red', label="y=1")
-ax[0].scatter(x_train[neg], y_train[neg], marker='o', s=100, label="y=0", facecolors='none', 
-              edgecolors=dlc["dlblue"],lw=3)
+    np.random.seed(1)
+X_tmp = np.random.rand(5,6)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1]).reshape(-1,)-0.5
+b_tmp = 0.5
+lambda_tmp = 0.7
+cost_tmp = compute_cost_linear_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
 
-ax[0].set_ylim(-0.08,1.1)
-ax[0].set_ylabel('y', fontsize=12)
-ax[0].set_xlabel('x', fontsize=12)
-ax[0].set_title('one variable plot')
-ax[0].legend()
+print("Regularized cost:", cost_tmp)
 
-#plot 2, two variables
-plot_data(X_train2, y_train2, ax[1])
-ax[1].axis([0, 4, 0, 4])
-ax[1].set_ylabel('$x_1$', fontsize=12)
-ax[1].set_xlabel('$x_0$', fontsize=12)
-ax[1].set_title('two variable plot')
-ax[1].legend()
-plt.tight_layout()
-plt.show()
+def compute_cost_logistic_reg(X, y, w, b, lambda_ = 1):
+    """
+    Computes the cost over all examples
+    Args:
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+    Returns:
+      total_cost (scalar):  cost 
+    """
 
-w_in = np.zeros((1))
-b_in = 0
-plt.close('all') 
-addpt = plt_one_addpt_onclick( x_train,y_train, w_in, b_in, logistic=False)
+    m,n  = X.shape
+    cost = 0.
+    for i in range(m):
+        z_i = np.dot(X[i], w) + b                                      #(n,)(n,)=scalar, see np.dot
+        f_wb_i = sigmoid(z_i)                                          #scalar
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)      #scalar
+             
+    cost = cost/m                                                      #scalar
 
+    reg_cost = 0
+    for j in range(n):
+        reg_cost += (w[j]**2)                                          #scalar
+    reg_cost = (lambda_/(2*m)) * reg_cost                              #scalar
+    
+    total_cost = cost + reg_cost                                       #scalar
+    return total_cost                                                  #scalar
+
+    np.random.seed(1)
+X_tmp = np.random.rand(5,6)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1]).reshape(-1,)-0.5
+b_tmp = 0.5
+lambda_tmp = 0.7
+cost_tmp = compute_cost_logistic_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
+
+print("Regularized cost:", cost_tmp)
+
+def compute_gradient_linear_reg(X, y, w, b, lambda_): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+      
+    Returns:
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar):       The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = X.shape           #(number of examples, number of features)
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+
+    for i in range(m):                             
+        err = (np.dot(X[i], w) + b) - y[i]                 
+        for j in range(n):                         
+            dj_dw[j] = dj_dw[j] + err * X[i, j]               
+        dj_db = dj_db + err                        
+    dj_dw = dj_dw / m                                
+    dj_db = dj_db / m   
+    
+    for j in range(n):
+        dj_dw[j] = dj_dw[j] + (lambda_/m) * w[j]
+
+    return dj_db, dj_dw
+        
+    np.random.seed(1)
+X_tmp = np.random.rand(5,3)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1])
+b_tmp = 0.5
+lambda_tmp = 0.7
+dj_db_tmp, dj_dw_tmp =  compute_gradient_linear_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
+
+print(f"dj_db: {dj_db_tmp}", )
+print(f"Regularized dj_dw:\n {dj_dw_tmp.tolist()}", )
+
+def compute_gradient_logistic_reg(X, y, w, b, lambda_): 
+    """
+    Computes the gradient for linear regression 
+ 
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+    Returns
+      dj_dw (ndarray Shape (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar)            : The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = X.shape
+    dj_dw = np.zeros((n,))                            #(n,)
+    dj_db = 0.0                                       #scalar
+
+    for i in range(m):
+        f_wb_i = sigmoid(np.dot(X[i],w) + b)          #(n,)(n,)=scalar
+        err_i  = f_wb_i  - y[i]                       #scalar
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err_i * X[i,j]      #scalar
+        dj_db = dj_db + err_i
+    dj_dw = dj_dw/m                                   #(n,)
+    dj_db = dj_db/m                                   #scalar
+
+    for j in range(n):
+        dj_dw[j] = dj_dw[j] + (lambda_/m) * w[j]
+
+    return dj_db, dj_dw  
+
+np.random.seed(1)
+X_tmp = np.random.rand(5,3)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1])
+b_tmp = 0.5
+lambda_tmp = 0.7
+dj_db_tmp, dj_dw_tmp =  compute_gradient_logistic_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
+
+print(f"dj_db: {dj_db_tmp}", )
+print(f"Regularized dj_dw:\n {dj_dw_tmp.tolist()}", )
+
+plt.close("all")
+display(output)
+ofit = overfit_example(True)
