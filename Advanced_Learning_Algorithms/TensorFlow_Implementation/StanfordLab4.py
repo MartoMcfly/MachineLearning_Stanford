b045f087044782d8901b6154d607ba2613f4cac1
MartoMcfly <martinpelaez@hotmail.es>
2022-10-30 22:54:24 +0100

    TensorFLow: Assignment 1

--- Advanced_Learning_Algorithms/TensorFlow_Implementation/StanfordLab4.py
+++ Advanced_Learning_Algorithms/TensorFlow_Implementation/StanfordLab4.py
@@ -1,17 +1,38 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
-plt.style.use('./deeplearning.mplstyle')
 import tensorflow as tf
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, LeakyReLU
-from tensorflow.python.keras.activations import linear, relu, sigmoid
-from matplotlib.widgets import Slider
-from lab_utils_common import dlc
-from autils import plt_act_trio
-from lab_utils_relu import *
-import warnings
-warnings.simplefilter(action='ignore', category=UserWarning)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.activations import linear, relu, sigmoid
+
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
+from public_tests import * 
+
+from autils import *
+from lab_utils_softmax import plt_softmax
+np.set_printoptions(precision=2)
 
 plt_act_trio()
-_ = plt_relu_ex()
+
+# UNQ_C1
+# GRADED CELL: my_softmax
+
+def my_softmax(z):  
+    """ Softmax converts a vector of values to a probability distribution.
+    Args:
+      z (ndarray (N,))  : input data, N features
+    Returns:
+      a (ndarray (N,))  : softmax of z
+    """    
+    ### START CODE HERE ### 
+
+    ### END CODE HERE ### 
+    return a
+
+
+
