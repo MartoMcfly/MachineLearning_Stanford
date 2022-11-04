0dcdf5e4df6c8dfb222b201072439475851d5926
MartoMcfly <martinpelaez@hotmail.es>
2022-11-04 16:58:16 +0100

    TensorFLow: Assignment 1 Extra

--- Advanced_Learning_Algorithms/TensorFlow_Implementation/C2_W2_Assignment.py
+++ Advanced_Learning_Algorithms/TensorFlow_Implementation/C2_W2_Assignment.py
@@ -34,41 +34,6 @@ def my_softmax(z):
     ### END CODE HERE ### 
     return a
 
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.activations import linear, relu, sigmoid
-
-import matplotlib.pyplot as plt
-plt.style.use('./deeplearning.mplstyle')
-
-import logging
-logging.getLogger("tensorflow").setLevel(logging.ERROR)
-tf.autograph.set_verbosity(0)
-
-from public_tests import * 
-
-from autils import *
-from lab_utils_softmax import plt_softmax
-np.set_printoptions(precision=2)
-
-plt_act_trio()
-
-# UNQ_C1
-# GRADED CELL: my_softmax
-
-def my_softmax(z):  
-    """ Softmax converts a vector of values to a probability distribution.
-    Args:
-      z (ndarray (N,))  : input data, N features
-    Returns:
-      a (ndarray (N,))  : softmax of z
-    """    
-    ### START CODE HERE ### 
-
-    ### END CODE HERE ### 
-    return a
 
 
 
